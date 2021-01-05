c63b12e7ca68e78f8caebe33cbcd3f3a96ade26d
saleh muhammad fasih <63059222+salehfasih@users.noreply.github.com>
2021-01-05 23:15:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// jquery basic syntax
+
+
+// $('#heading1').hide();
+// $('p span').css('color','red');
+// $('.para2').css('background-color','yellow');
+
+$('ul#list  li:odd ').css('background-color','orange');
+$('ul#list  li:even ').css('background-color','grey');
+$('ul#list  li ').css('list-style','none');
+$('ul#list  li ').css('display','inline-flex',);
+$('ul ').css('justify-content','center',);
+$('ul#list  li ').css('margin-left','10px',);
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+var citrus = fruits.slice(1, 3);
+console.log(citrus);
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+console.log(fruits);
+
+
+// for input
+
+// $(':button').css('background-color','green');
+// $("input").hover(function(){
+//     $(this).css("background-color", "yellow");
+//     }, function(){
+//     $(this).css("background-color", "white");
+//   });
+
+//   $('a[href="http://google.com"]').css('color','red');
+
+
+
